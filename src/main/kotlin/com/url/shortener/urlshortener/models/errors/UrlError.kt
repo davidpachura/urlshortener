b87a0c4735c490546e76e298ea71ff36b13a7625
David Pachura <david.pachura@gmail.com>
2025-10-29 22:25:35 -0500

    Add UrlError class for future error handling

---
+++ src/main/kotlin/com/url/shortener/urlshortener/models/errors/UrlError.kt
@@ -0,0 +1,9 @@
+package com.url.shortener.urlshortener.models.errors
+
+sealed class UrlError {
+    data class InvalidUrl(val message: String) : UrlError()
+    data class UrlNotFound(val message: String) : UrlError()
+    data class Conflict(val message: String) : UrlError()
+    data class Unexpected(val message: String) : UrlError()
+    data class BadRequest(val message: String) : UrlError()
+}
